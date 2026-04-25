f94ac32e686160e856d8541fcc3fa362379c88f7
sebaz0x <hamdanlag222@gmail.com>
2026-04-25 16:42:02 +0700

    masih oke

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,13 @@
+let angka = 0
+
+while (angka < 10) {
+    console.log(`loop: ${angka}`)
+    angka++
+};
+
+let angka2 = 10
+
+while (angka2 > 0) {
+    console.log(`loopmundur: ${angka2}`)
+    angka2--
+};
---
+++ quiz/part3/soal2.js
@@ -0,0 +1,8 @@
+for (let angka = 1; angka <= 10; angka++){
+    console.log(`looppertama: ${angka}`)
+};
+
+
+for (let angka2 = 10; angka2 >= 1; angka2--){
+    console.log(`loopkedua: ${angka2}`)
+};
