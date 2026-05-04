5dcd2b89cd13ff5f80322840bf8aa9efdaf2c80a
sebaz0x <hamdanlag222@gmail.com>
2026-05-04 13:35:43 +0700

    part4 belom

--- quiz/part3/soal5.js
+++ quiz/part3/soal5.js
@@ -1,13 +1,13 @@
 let input = 5
-let bintang = ''
+
 
 for (let i = 1; i <= input; i++){
     for (let j = 1; j <= i; j++){
-        bintang += '*'
+        console.log('*')
     }
-    bintang += '\n'
+    console.log('/n')
 }
-console.log(bintang)
+
 
 
 
